aac47e2155049ba030836eb0c95de3e79e13d227
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-01-29 14:00:52 +0000

    yes

--- app/main.py
+++ app/main.py
@@ -8,8 +8,6 @@ st.write("Welcome to the WhatsApp Chat app. This app visualizes your whatsapp ch
             "All the processing and analysis is done in the browser and your data is not sent to any other source"
 )
 
-st.set_page_config(page_title="File Upload", page_icon=":clipboard:", layout="wide")
-
 uploaded_file = st.file_uploader("Upload a file", type="txt")
     
 if uploaded_file is not None:
