034ab4715625b65fe417ba760ce39d3e182f1d4e
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-01-29 14:02:19 +0000

    .

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ st.write("Welcome to the WhatsApp Chat app. This app visualizes your whatsapp ch
             "All the processing and analysis is done in the browser and your data is not sent to any other source"
 )
 
-uploaded_file = st.file_uploader("Upload a file", type="txt")
+# uploaded_file = st.file_uploader("Upload a file", type="txt")
     
-if uploaded_file is not None:
-    st.write("File Uploaded")
+# if uploaded_file is not None:
+#     st.write("File Uploaded")
