74be3095c72795a92e5bfcf98164f58bb46e9253
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-01-29 16:41:26 +0000

    .

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ if uploaded_file is not None:
                     f"lasting **{(df['date'].iloc[-1]-df['date'].iloc[0]).days}** days."
                 )
                 authors = uf.authors_chat_count(df)
-                st.write(type(authors.index))
+                st.write(type(authors.index.astype(str)))
                 fig = px.bar(authors, orientation='h')
                 st.plotly_chart(fig)
                 
