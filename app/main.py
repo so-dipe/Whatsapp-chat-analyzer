7d30bed871e54b2674e2a18d27c399a58b8a680a
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-01-29 15:56:20 +0000

    .

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ if uploaded_file is not None:
                 )
                 authors = uf.authors_chat_count(df).values
                 st.write(authors)
-                st.bar_chart(y=authors, x=authors.index)
+                st.bar_chart(y=authors.values, x=authors.index)
                 
 
             
