56475fb51bb83e952c6898b3300207d357f424d2
sodipe <paulsodipe@gmail.com>
2023-02-02 21:48:07 +0000

    lets see if it works

--- app/main.py
+++ app/main.py
@@ -71,6 +71,7 @@ if uploaded_file is not None:
                     st.write(
                         'You can check the activity of all members in Participants'
                     )
+                    print('')
                     
                 
             with tab2:
